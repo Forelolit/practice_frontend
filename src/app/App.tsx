df3541657ecff002173c7e4946f49b1bf98b1c5c
Your Name <you@example.com>
2025-06-04 18:05:31 +0600

    Keep calm and commit

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import { CustomInput } from 'shared/customInput';
+
+function App() {
+  return (
+    <div
+      style={{
+        margin: '0 auto',
+        width: 1000,
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '20px 0',
+      }}
+    >
+      <CustomInput type="text" placeholder="Input" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/shared/customInput/CustomInput.tsx
@@ -0,0 +1,20 @@
+import { useState, type HTMLInputTypeAttribute } from 'react';
+import styles from './index.module.scss';
+
+interface CustomInputProps {
+  type: HTMLInputTypeAttribute;
+  placeholder?: string;
+}
+
+export const CustomInput = ({ type, placeholder }: CustomInputProps) => {
+  const [value, setValue] = useState('');
+  return (
+    <input
+      className={`${styles.base} ${styles.input}`}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
+};
